ffd6eb9d68e7c2a0357536c8ef7b7cccb9e5202e
Natalia Levenets <levenets.lot@gmail.com>
2023-03-19 16:58:53 +0200

    first commit logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,92 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-
 import storage from '../helpers/storage';
-
-import { PhonebookForm } from '../components/PhonebookForm/PhonebookForm';
-import { Filter } from '../components/Filter/Filter';
-import { ContactsList } from '../components/ContactsList/ContactsList';
-
-import css from '../components/PhonebookForm/PhonebookForm.module.css';
-
-const DEFAULTCONTACTS = [
-  {
-    id: 'id-1',
-    name: 'Rosie Simpson',
-    number: '459-12-56',
-  },
-  {
-    id: 'id-2',
-    name: 'Hermione Kline',
-    number: '443-89-12',
-  },
-  {
-    id: 'id-3',
-    name: 'Eden Clements',
-    number: '645-17-79',
-  },
-  {
-    id: 'id-4',
-    name: 'Annie Copeland',
-    number: '227-91-26',
-  },
-];
+import css from '../index.css';
 
 export function App () {
 
-const [contacts, setContacts] = useState(()=>storage.load('contacts-list') ?? DEFAULTCONTACTS);
-const [filter, setFilter] = useState('');
+const [currentState, setCurrentState] = useState(()=>storage.load('currentState') ?? false);
+const [followersCounter, setFollowersCounter] = useState(()=>storage.load('followersCounter') ?? 100500);
+
+const toggleCounterState = () => {
+console.log("before click", currentState)
+setCurrentState(!currentState);
+setFollowersCounter((!currentState) ? (followersCounter + 1) : (followersCounter) - 1);
+};
+
 
 useEffect(() => {
-  storage.save('contacts-list', contacts);
-}, [contacts]);
-
-const addContact = contact => {
-    const isExist = contacts.find(({ name }) => {
-      return contact.name === name;
-    });
-    if (isExist) {
-      alert(`${contact.name} is already in contacts`);
-      return;
-    } else {
-      setContacts((prevState) => [...prevState, {...contact, id: nanoid()}]);
-    }
-  };
-  
-
-
-  const handleFilter = ({ target: { value } }) => {
-    setFilter( value );
-  };
-
-  const getFilteredContacts = () => {
-    const normaliseFilter = filter.trim().toLowerCase();
-    return contacts.filter(contact => {
-    return contact.name.toLowerCase().includes(normaliseFilter);
-    });
-  };
-
-  const deleteContacts = e => {
-    setContacts(prevState => prevState.filter(contact => contact.id !== e.target.id))
-  };
-
-    const filteredContacts = getFilteredContacts();
+  storage.save('currentState', currentState); 
+  storage.save('followersCounter', followersCounter); 
+}, [currentState, followersCounter]);
 
     return (
     <>
-     <h2 className={css.phonebookTitle}>Phonebook</h2>
-        <PhonebookForm onSubmit={addContact} />
-        <div className={css.phonebookContactsList}>
-          <h2 className={css.phonebookListTitle}>Contacts</h2>
-          <Filter onChange={handleFilter} value={filter} />
+        <div>
+          <ul>
+            <li>
+              <p>followers</p>
+              <p>{followersCounter}</p>
+            </li>
+          </ul>
+          <button className={css.btnSubmit} type='button' onClick={toggleCounterState}>add</button>
         </div>
-        {contacts.length > 0 && (
-          <ContactsList
-            filteredContacts={filteredContacts}
-            onBtnDelete={deleteContacts}
-          />
-        )}
+
     </>
     );
 }
-    
+
+/* <div className={currentState ? [css.btnSubmit, css.active].join(' ') : css.btnSubmit}> */
